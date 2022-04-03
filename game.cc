e1cedb2f83fc0dcdda8b1dde2bb2c9d026a73b8e
d279liu <d279liu@uwaterloo.ca>
2022-04-02 22:57:58 -0400

    updated game

--- game.cc
+++ game.cc
@@ -1,3 +1,21 @@
 #include "game.h"
 #include "board.h"
 #include <utility>
+
+using namespace std;
+
+void Game::start() {}
+
+// char getState(int x, int y) { return 0; }
+
+shared_ptr<Board> Game::getBoard() { return nullptr; }
+
+void Game::winner() {}
+
+void Game::getTurn() {}
+
+void Game::nextTurn() {}
+
+void Game::updateOutput() {}
+
+void Game::displayGame() {}
